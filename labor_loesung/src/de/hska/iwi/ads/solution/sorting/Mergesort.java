6716169204f03e3755f2884eb4d07fd9809416de
cptdavenport <trongkhuengo@gmail.com>
2023-04-26 21:49:19 +0200

    Added file Mergesort

---
+++ labor_loesung/src/de/hska/iwi/ads/solution/sorting/Mergesort.java
@@ -0,0 +1,14 @@
+package de.hska.iwi.ads.solution.sorting;
+
+import de.hska.iwi.ads.sorting.AbstractMergesort;
+import de.hska.iwi.ads.sorting.Sort;
+
+public class Mergesort<E extends Comparable<E>> implements Sort<E>{
+
+	@Override
+	public void sort(E[] a) {
+	
+		
+	}
+
+}

00abea669083b5d13a38ee454f56018d6f4ca920
cptdavenport <trongkhuengo@gmail.com>
2023-04-26 20:34:51 +0200

    Changes to .metadata, created new BinarySearch class

---
+++ labor_loesung/src/de/hska/iwi/ads/solution/search/BinarySearch.java
@@ -0,0 +1,15 @@
+package de.hska.iwi.ads.solution.search;
+
+import de.hska.iwi.ads.search.Search;
+
+public class BinarySearch implements Search {
+
+	@Override
+	public int search(Comparable[] a, Comparable key, int left, int right) {
+		
+		return 0;
+	}
+	
+	
+
+}

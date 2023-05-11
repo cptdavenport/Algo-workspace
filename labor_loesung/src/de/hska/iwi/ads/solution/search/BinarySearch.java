2ced0431aee542d34d2286fb0a19ccc6ab2a1aab
cptdavenport <trongkhuengo@gmail.com>
2023-05-12 01:05:16 +0200

    changes to binary search

--- labor_loesung/src/de/hska/iwi/ads/solution/search/BinarySearch.java
+++ labor_loesung/src/de/hska/iwi/ads/solution/search/BinarySearch.java
@@ -4,53 +4,53 @@ import de.hska.iwi.ads.search.Search;
 
 public class BinarySearch<E extends Comparable<E>> implements Search<E> {
 
-    @Override
-    public int search(E[] a, E key, int left, int right) {
-        // Check for null input array
-        if (a == null) {
-            throw new NullPointerException("Input array is null");
-        }
+	@Override
+	public int search(E[] a, E key, int left, int right) {
+		// Check for null input array
+		if (a == null) {
+			throw new NullPointerException("Input array is null");
+		}
 
-        // Check for invalid indices
-        if (left < 0 || left >= a.length || right < 0 || right >= a.length || left > right) {
-            throw new ArrayIndexOutOfBoundsException("Invalid left or right index");
-        }
+		// Check for invalid indices
+		if (left < 0 || left >= a.length || right < 0 || right >= a.length || left > right) {
+			throw new ArrayIndexOutOfBoundsException("Invalid left or right index");
+		}
 
-        int low = left;
-        int high = right;
-        int result = -1; // initialize result to -1
+		if (key.compareTo(a[left]) < 0) { // case 1
+			return left - 1;
+		} else if (key.compareTo(a[right]) > 0) { // case 2
+			return right + 1;
+		}
 
-        while (low <= high) {
-            int mid = (low + high) >>> 1; // calculate mid point
-            E midVal = a[mid];
-            int cmp = midVal.compareTo(key);
+		int low = left;
+		int high = right;
+		int result = -1; // initialize result to -1
 
-            if (cmp < 0) {
-                low = mid + 1;
-            } else if (cmp > 0) {
-                high = mid - 1;
-            } else { // key found at index mid
-                result = mid;
-                high = mid - 1; // keep searching for lower index of key
-            }
-        }
+		while (low <= high) {
+			int mid = (low + high) >>> 1; // calculate mid point
+			E midVal = a[mid];
+			int cmp = midVal.compareTo(key);
 
-        // key not found, check for other cases
-        if (result == -1) {
-            if (key.compareTo(a[left]) < 0) { // case 1
-                result = left - 1;
-            } else if (key.compareTo(a[right]) > 0) { // case 2
-                result = right + 1;
-            } else { // case 3
-                for (int i = left; i <= right; i++) {
-                    if (key.compareTo(a[i]) < 0) {
-                        result = i;
-                        break;
-                    }
-                }
-            }
-        }
+			if (cmp < 0) {
+				low = mid + 1;
+			} else if (cmp > 0) {
+				high = mid - 1;
+			} else { // key found at index mid
+				result = mid;
+				high = mid - 1; // keep searching for lower index of key
+			}
+		}
 
-        return result;
-    }
+		// key not found, case 3
+		if (result == -1) {
+			for (int i = left; i <= right; i++) {
+				if (key.compareTo(a[i]) < 0) {
+					result = i;
+					break;
+				}
+			}
+		}
+
+		return result;
+	}
 }
--- labor_loesung/src/de/hska/iwi/ads/solution/search/BinarySearchTest.java
+++ labor_loesung/src/de/hska/iwi/ads/solution/search/BinarySearchTest.java
@@ -20,55 +20,54 @@ class BinarySearchTest extends SearchTest {
 	public <E extends Comparable<E>> Search<E> createSearch() {
 		return new BinarySearch<E>();
 	}
-	
+
 	@Test
 	void testSearchNonexistentElementInMiddleOfArray() {
 		Search<Integer> search = createSearch();
-	    Integer[] array = {1, 3, 5, 7, 9};
-	    int result = search.search(array, 4);
-	    assertEquals(2, result);
+		Integer[] array = { 1, 3, 5, 7, 9 };
+		int result = search.search(array, 4);
+		assertEquals(2, result);
 	}
 
 	@Test
 	void testSearchNonexistentElementAtBeginningOfArray() {
 		Search<Integer> search = createSearch();
-	    Integer[] array = {1, 3, 5, 7, 9};
-	    int result = search.search(array, 0);
-	    assertEquals(-1, result);
+		Integer[] array = { 1, 3, 5, 7, 9 };
+		int result = search.search(array, 0);
+		assertEquals(-1, result);
 	}
 
 	@Test
 	void testSearchNonexistentElementAtEndOfArray() {
 		Search<Integer> search = createSearch();
-	    Integer[] array = {1, 3, 5, 7, 9};
-	    int result = search.search(array, 10);
-	    assertEquals(5, result);
+		Integer[] array = { 1, 3, 5, 7, 9 };
+		int result = search.search(array, 10);
+		assertEquals(5, result);
 	}
 
 	@Test
 	void testSearchEmptyArray() {
 		Search<Integer> search = createSearch();
-	    Integer[] array = {};
-	    int result = search.search(array, 2);
-	    assertEquals(-1, result);
+		Integer[] array = {};
+		assertThrows(ArrayIndexOutOfBoundsException.class, () -> search.search(array, 2) );
 	}
 
 	@Test
 	void testSearchSingleElementArray() {
 		Search<Integer> search = createSearch();
-	    Integer[] array = {4};
-	    int result = search.search(array, 4);
-	    assertEquals(0, result);
+		Integer[] array = { 4 };
+		int result = search.search(array, 4);
+		assertEquals(0, result);
 	}
 
 	@Test
 	void testSearchRepeatedElements() {
 		Search<Integer> search = createSearch();
-	    Integer[] array = {1, 3, 3, 5, 7, 7, 9};
-	    int result1 = search.search(array, 3);
-	    int result2 = search.search(array, 7);
-	    assertEquals(1, result1);
-	    assertEquals(4, result2);
+		Integer[] array = { 1, 3, 3, 3, 3, 5, 7, 7, 9 };
+		int result1 = search.search(array, 3);
+		int result2 = search.search(array, 7);
+		assertEquals(1, result1);
+		assertEquals(6, result2);
 	}
 
 }
